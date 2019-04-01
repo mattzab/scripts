07e541707fa56a415bcaad242479b03c6a9d3ec1
mattzab <mattzab@gmail.com>
2019-04-01 12:00:42 -0500

    Create crouton.sh

---
+++ bash/crouton.sh
@@ -0,0 +1,2 @@
+curl -o ~/Downloads/crouton https://raw.githubusercontent.com/dnschneid/crouton/master/installer/crouton
+sudo sh ~/Downloads/crouton -t xiwi,touch,extension,cli-extra,audio -n ubuntu
