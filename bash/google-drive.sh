4ec51562b39c8f102e37b3070f32c2ccd626ea86
mattzab <mattzab@gmail.com>
2019-09-27 12:40:52 -0500

    Update google-drive.sh

--- bash/google-drive.sh
+++ bash/google-drive.sh
@@ -1,23 +1,26 @@
-sleep 3
+sleep 1
 echo "Run in tmux if you want to close this window!"
 echo " "
-sleep 3
+sleep 1
 echo "What would you like your Google Drive folder to be called?"
 read gdrivef
-sleep 3
+mkdir $HOME/.tempgdrive $HOME/.tempgdrive/remote $HOME/.tempgdrive/local $HOME/"$gdrivef" 
 echo " "
-echo "Your Google Drive folder will be created at ~/'$gdrivef'"
-sleep 3
+echo "Your Google Drive folder was created at ~/'$gdrivef'"
+sleep 1
 echo " "
 echo "Which rclone remote would you like to use?"
 rclone listremotes
+echo " "
 read rcloneremote
-sleep 3
+sleep 1
 echo " "
 echo "Cloud and local data will synchronize every 1 minute."
 echo " "
 echo "Starting Google Drive at '$gdrivef' using mount '$rcloneremote'."
-mkdir ~/.tempgdrive ~/.tempgdrive/remote ~/.tempgdrive/local "~/$gdrivef" 
-rclone mount "$rcloneremote" "~/$gdrivef" --vfs-cache-mode writes
-mergerfs -o rw,async_read=false,use_ino,allow_other,func.getattr=newest,category.action=all,category.create=ff,cache.files=partial,dropcacheonclose=true ~/.tempgdrive/local:~/.tempgdrive/remote "~/$gdrivef" 
-while :; do rclone move ~/.tempgdrive/local "$rcloneremote" -P; sleep $time; done
+
+/usr/bin/rclone mount "$rcloneremote" $HOME/.tempgdrive/remote --vfs-cache-mode writes &
+/usr/bin/mergerfs -o rw,async_read=false,use_ino,allow_other,func.getattr=newest,category.action=all,category.create=ff,cache.files=partial,dropcacheonclose=true $HOME/.tempgdrive/local:$HOME/.tempgdrive/remote $HOME/"$gdrivef" 
+while :; do /usr/bin/rclone move $HOME/.tempgdrive/local "$rcloneremote" -P; /bin/sleep 1m; done
+fusermount -u $HOME/.tempgdrive/remote
+fusermount -u $HOME/"$gdrivef"
